c2a79a7ebd866095eb90f226dc7c9a314d2ed63e
viccycg <viccyg@digitalpharos.com>
2018-12-04 15:16:49 -0700

    Moved Stripe handling to tickets.service.ts

--- src/app/components/tickets/tickets.component.ts
+++ src/app/components/tickets/tickets.component.ts
@@ -1,19 +1,7 @@
 import {
   Component,
-  OnInit,
-  AfterViewInit,
-  OnDestroy,
-  ViewChild,
-  ElementRef,
-  ChangeDetectorRef } from '@angular/core';
+  OnInit } from '@angular/core';
 import { TicketsService } from '../../services/tickets/tickets.service';
-// import { NgForm } from '@angular/forms';
-// import * as Stripe from 'stripe';
-// const stripe = new Stripe('pk_test_txJbZH4ucp67IJiIvkyzixCh');
-// const elements = stripe.elements();
-
-// const customer: Promise<Stripe.customers.ICustomer> = stripe.customers.create(/* ... */);
-// ```
 
 @Component({
   selector: 'app-tickets',
@@ -21,16 +9,6 @@ import { TicketsService } from '../../services/tickets/tickets.service';
   styleUrls: ['./tickets.component.css']
 })
 export class TicketsComponent implements OnInit {
-  // export class TicketsComponent implements OnInit, AfterViewInit, OnDestroy {
-
-  // @ViewChild('cardInfo') cardInfo: ElementRef;
-  // card: any;
-  // cardHandler = this.onChange.bind(this);
-  // error: string;
-  // const stripe = Stripe('pk_test_txJbZH4ucp67IJiIvkyzixCh'); // use your test publishable key
-  // console.log('stripe: ', stripe);
-
-  // TODO: figure this Stripe script location out - currently in angular.json??
 
   public tickets: any[];
   public cacheTickets: any[];
@@ -39,7 +17,6 @@ export class TicketsComponent implements OnInit {
   public buy: boolean = false;
 
   constructor(private ticketService: TicketsService) {
-    // constructor(private ticketService: TicketsService, private changeDetector: ChangeDetectorRef) {
     this.ticketService.getTickets()
       .subscribe(tickets => {
         this.tickets = tickets.data;
@@ -51,37 +28,6 @@ export class TicketsComponent implements OnInit {
   ngOnInit() {
   }
 
-  // ngAfterViewInit() {
-  //   this.card = elements.create('card');
-  //   this.card.mount(this.cardInfo.nativeElement);
-  //
-  //   this.card.addEventListener('change', this.cardHandler);
-  // }
-  //
-  // ngOnDestroy() {
-  //   this.card.removeEventListener('change', this.cardHandler);
-  //   this.card.destroy();
-  // }
-  //
-  // onChange({ error }) {
-  //   if (error) {
-  //     this.error = error.message;
-  //   } else {
-  //     this.error = null;
-  //   }
-  //   this.changeDetector.detectChanges();
-  // }
-
-  // async onSubmit(form: NgForm) {
-  //   const { token, error } = await stripe.createToken(this.card);
-  //
-  //   if (error) {
-  //     console.log('Something is wrong: ', error);
-  //   } else {
-  //     console.log('Success! ', token);
-  //   }
-  // }
-
   getTickets(filterVal: any) {
     this.show = true;
     if (filterVal === '0') {
@@ -92,25 +38,7 @@ export class TicketsComponent implements OnInit {
   }
 
   purchaseTickets() {
-    // this.ticketService.purchaseTickets();
-    // this.buy = true;
-    console.log('in purchaseTickets()');
-
-    const handler = (<any>window).StripeCheckout.configure({
-      key: 'pk_test_txJbZH4ucp67IJiIvkyzixCh',
-      locale: 'auto',
-      token: function (token: any) {
-        // You can access the token ID with `token.id`.
-        // Get the token ID to your server-side code for use.
-      }
-    });
-
-    handler.open({
-      name: 'TixHelp',
-      description: 'Ticket Purchase',
-      amount: 6000,
-      currency: 'cad'
-    });
-
+    this.ticketService.purchaseTickets();
+    // this.show = false;
   }
 }
--- src/app/services/tickets/tickets.service.ts
+++ src/app/services/tickets/tickets.service.ts
@@ -26,13 +26,22 @@ export class TicketsService {
   }
 
   purchaseTickets() {
-    // const charge = stripe.charges.create({
-    //   amount: 999,
-    //   currency: 'usd',
-    //   source: 'tok_visa',
-    //   receipt_email: 'viccyc@hotmail.com',
-    // });
+    const handler = (<any>window).StripeCheckout.configure({
+      key: 'pk_test_txJbZH4ucp67IJiIvkyzixCh',
+      locale: 'auto',
+      token: function (token: any) {
+        // You can access the token ID with `token.id`.
+        // Get the token ID to your server-side code for use.
+        console.log('token: ', token);
+      }
+    });
 
-    // console.log('charge: ', charge);
+    handler.open({
+      name: 'TixHelp',
+      description: 'Ticket Purchase',
+      amount: 6000,
+      currency: 'cad'
+    });
+
   }
 }
