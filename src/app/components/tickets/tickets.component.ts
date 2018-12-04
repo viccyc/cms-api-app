3ec3d087408adb8ca69cf6b03c2545aac7a28a98
viccycg <viccyg@digitalpharos.com>
2018-12-04 12:10:46 -0700

    Setting up integration with Stripe

--- src/app/components/tickets/tickets.component.ts
+++ src/app/components/tickets/tickets.component.ts
@@ -1,5 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  AfterViewInit,
+  OnDestroy,
+  ViewChild,
+  ElementRef,
+  ChangeDetectorRef } from '@angular/core';
 import { TicketsService } from '../../services/tickets/tickets.service';
+// import { NgForm } from '@angular/forms';
+// import * as Stripe from 'stripe';
+// const stripe = new Stripe('pk_test_txJbZH4ucp67IJiIvkyzixCh');
+// const elements = stripe.elements();
+
+// const customer: Promise<Stripe.customers.ICustomer> = stripe.customers.create(/* ... */);
+// ```
 
 @Component({
   selector: 'app-tickets',
@@ -7,13 +21,25 @@ import { TicketsService } from '../../services/tickets/tickets.service';
   styleUrls: ['./tickets.component.css']
 })
 export class TicketsComponent implements OnInit {
+  // export class TicketsComponent implements OnInit, AfterViewInit, OnDestroy {
+
+  // @ViewChild('cardInfo') cardInfo: ElementRef;
+  // card: any;
+  // cardHandler = this.onChange.bind(this);
+  // error: string;
+  // const stripe = Stripe('pk_test_txJbZH4ucp67IJiIvkyzixCh'); // use your test publishable key
+  // console.log('stripe: ', stripe);
+
+  // TODO: figure this Stripe script location out - currently in angular.json??
 
   public tickets: any[];
   public cacheTickets: any[];
   public ticketNames: any;
   public show: boolean = false;
+  public buy: boolean = false;
 
   constructor(private ticketService: TicketsService) {
+    // constructor(private ticketService: TicketsService, private changeDetector: ChangeDetectorRef) {
     this.ticketService.getTickets()
       .subscribe(tickets => {
         this.tickets = tickets.data;
@@ -25,6 +51,37 @@ export class TicketsComponent implements OnInit {
   ngOnInit() {
   }
 
+  // ngAfterViewInit() {
+  //   this.card = elements.create('card');
+  //   this.card.mount(this.cardInfo.nativeElement);
+  //
+  //   this.card.addEventListener('change', this.cardHandler);
+  // }
+  //
+  // ngOnDestroy() {
+  //   this.card.removeEventListener('change', this.cardHandler);
+  //   this.card.destroy();
+  // }
+  //
+  // onChange({ error }) {
+  //   if (error) {
+  //     this.error = error.message;
+  //   } else {
+  //     this.error = null;
+  //   }
+  //   this.changeDetector.detectChanges();
+  // }
+
+  // async onSubmit(form: NgForm) {
+  //   const { token, error } = await stripe.createToken(this.card);
+  //
+  //   if (error) {
+  //     console.log('Something is wrong: ', error);
+  //   } else {
+  //     console.log('Success! ', token);
+  //   }
+  // }
+
   getTickets(filterVal: any) {
     this.show = true;
     if (filterVal === '0') {
@@ -32,5 +89,10 @@ export class TicketsComponent implements OnInit {
     } else {
       this.tickets = this.cacheTickets.filter((item) => item.ticketName === filterVal);
     }
+  }
+
+  purchaseTickets() {
+    this.ticketService.purchaseTickets();
+    this.buy = true;
   }
 }
--- src/app/services/tickets/tickets.service.ts
+++ src/app/services/tickets/tickets.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 
+
 interface JSONObject {
   _id: string;
   ticketName: string;
@@ -22,5 +23,16 @@ export class TicketsService {
 
   getTickets() {
     return this.http.get<JSONObject>(this.apiUrl, {headers});
+  }
+
+  purchaseTickets() {
+    // const charge = stripe.charges.create({
+    //   amount: 999,
+    //   currency: 'usd',
+    //   source: 'tok_visa',
+    //   receipt_email: 'viccyc@hotmail.com',
+    // });
+
+    console.log('charge: ', charge);
   }
 }
